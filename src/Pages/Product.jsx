1c76437a54b56672b82d0b45a35c2ab264be81d0
ankitbhandary24 <ankitbhandary2005@gmail.com>
2024-07-04 21:35:03 +0530

    Products can be seen now

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {ShopContext} from '../Context/ShopContext'
+import { useParams } from 'react-router-dom'
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+
 const Product = () => {
+    const { all_product } =useContext(ShopContext);
+    const {productId} = useParams();
+    console.log (productId)
+    const product = all_product.find((e)=> e.id === Number(productId));
     return (
         <div>
-            
+            {console.log (product)}
+            {/* product Product */}
+            <Breadcrum product={product} /> 
+            <ProductDisplay product={product}/>
         </div>
     )
 }
