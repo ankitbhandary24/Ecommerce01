5438c4a277f6188561c0e8191fae5f9c6a441b83
Sayantan Sarkar <sayantan2002sarkar@gmail.com>
2024-07-24 12:37:30 +0530

    Done by Sayantan

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -5,7 +5,7 @@ const LoginSignup = () => {
         <div className='loginsignup'>
             <div className="loginsignup-container">
                 <h1>Sign Up</h1>
-                <div className="loginsignup-fie1ds">
+                <div className="loginsignup-fields">
                     < input type="text" placeholder='Your Name' />
                     < input type="email" placeholder=' Email Address' />
                     < input type="password" placeholder='Password' />
